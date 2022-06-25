a1dc9767d0a17b5fb1aa71d75d959300a346f7b9
Archer <9527.code@gmail.com>
2022-06-25 09:40:44 +0800

    增加toref

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,11 @@ const routes: Array<RouteRecordRaw> = [
         name: "Reactive",
         component: ()=> import("../views/Reactive/index.vue")
     },
+    {
+        path: '/toref',
+        name: "Toref",
+        component: ()=> import("../views/ToRef/index.vue")
+    },
 ];
 
 const router = createRouter({
@@ -24,13 +29,13 @@ const router = createRouter({
     * 基于h5的history实现
     * 通过 window.addEventListener('popstate', (e) => {console.log(e)}) 监听浏览器前进后退  e.state.forward、e.state.back
     * */
-    history: createWebHistory(), //
+    // history: createWebHistory(), //
     /*
     * hash 模式: 端口号后带#号
     * 通过 location.hash 切换路由
     * 原理是通过 window.addEventListener('hashchange', () => {}) 监听浏览器前进后退  e.newURL、e.oldURL
     * */
-    // history: createWebHashHistory(),
+    history: createWebHashHistory(),
     routes
 });
 
---
+++ src/views/ToRef/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="to-ref">
+2
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref, Ref, isRef, shallowRef, triggerRef, customRef} from 'vue';
+
+</script>
+
+<style>
+
+</style>
